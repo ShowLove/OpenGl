428e365dce0fea54c1f254396555de7211a36532
Carlos <garzoncarlosandres@gmail.com>
2019-10-16 02:21:12 -0400

    added GLEW functionality

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,7 +1,11 @@
+#define GLEW_STATIC 1 // Removes error defaultlib 'MSVCRT' conflicts with use of other libs; use /NODEFAULTLIB:library
+					  // Since we are not dynamic linking we need this
+
 //#include <GLFW\include\glfw3.h>
 //#include <Dependencies/GLFW/include/glfw3.h>
-#include <../Dependencies/GLFW/include/GLFW/glfw3.h>
 //#include <GLFW/glfw3.h>
+#include <../Dependencies/GLEW/include/GL/glew.h>
+#include <../Dependencies/GLFW/include/GLFW/glfw3.h>
 //#include <glofw3.h>
 
 #include <stdio.h>  /* defines FILENAME_MAX */
@@ -28,7 +32,7 @@ int main(void)
 	// Initialize the library
 	if (!glfwInit())
 		return -1;
-
+	
 	// Create a windowed mode window and its OpenGL context 
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
@@ -36,9 +40,15 @@ int main(void)
 		glfwTerminate();
 		return -1;
 	}
-
+	
 	// Make the window's context current 
 	glfwMakeContextCurrent(window);
+
+	// We need to call glewInit after we have a valid context
+	if(glewInit() != GLEW_OK)
+		std::cout << "Error !GLEW_OK" << std::endl;
+	
+	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
 	// Loop until the user closes the window 
 	while (!glfwWindowShouldClose(window))
